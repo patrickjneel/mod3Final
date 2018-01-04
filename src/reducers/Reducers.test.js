7996c5c088007fa8d01591848cf7fbc6777eb498
Pat Neel <patrick.neel@colorado.edu>
2018-01-04 11:14:47 -0700

    added reducer test

--- src/reducers/Reducers.test.js
+++ src/reducers/Reducers.test.js
@@ -1,2 +1,10 @@
-import * as actions from 
+import * as actions from '../actions/index.js';
 import { addHousesReducer } from './fake-reducer';
+
+describe('reducer tests', () => {
+
+  it.only('should return the default state', () => {
+    const expected = [];
+    expect(addHousesReducer(undefined, {})).toEqual(expected)
+  })
+})
